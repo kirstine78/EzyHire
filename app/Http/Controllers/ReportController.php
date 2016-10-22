e9b41bef32c33f618c13d535a401eb144d2afc92
kirsti <kirstine78@gmail.com>
2016-10-22 20:07:14 +1100

    regarding show damages report, put big query of join tables and union into separate function

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -24,6 +24,51 @@ class ReportController extends Controller
 {
     public function showDamagesReport(Request $request){
 
+        $unionTable = $this->getFixedAndUnFixedDamages();
+
+//        // get all damages belonging to NON-archived bookings, and car details
+//        $joinTableNonArchivedBookingVehicleDamage = DB::table('bookings')
+//            ->join('vehicles', 'vehicles.id', '=', 'bookings.fldCarId')
+//            ->join('damages', 'bookings.id', '=', 'damages.fldBookingNo')
+//            ->where('vehicles.fldRetired', '=', 0)
+//            ->select('vehicles.fldRegoNo as regoNo',
+//                'damages.fldDamageDate as damageDate',
+//                'damages.fldDamageType as damageType',
+//                'damages.fldDamageDescription as damageDescription',
+//                'damages.fldFixed as fixed');
+//
+//        // get all damages belonging to archived bookings, and car details, and union it with $joinTableNonArchivedBookingVehicleDamage
+//        $unionTable = DB::table('archivedbookings')
+//            ->join('vehicles', 'vehicles.id', '=', 'archivedbookings.fldCarId')
+//            ->join('archiveddamages', 'archivedbookings.id', '=', 'archiveddamages.fldArchiveBookingNo')
+//            ->where('vehicles.fldRetired', '=', 0)
+//            ->select('vehicles.fldRegoNo as regoNo',
+//                'archiveddamages.fldDamageDate as damageDate',
+//                'archiveddamages.fldDamageType as damageType',
+//                'archiveddamages.fldDamageDescription as damageDescription',
+//                'archiveddamages.fldFixed as fixed')
+//            ->union($joinTableNonArchivedBookingVehicleDamage)
+//            ->orderBy('regoNo', 'asc')
+//            ->orderBy('damageDate', 'desc')
+//            ->get();
+
+        if ($request->radFilterDamages == null)
+        {
+            $filterOption = "fixedAndUnFixed";
+        }
+        else
+        {
+            $filterOption = $request->radFilterDamages;
+        }
+
+        return View('report.showDamagesReport', ['unionTableArchivedAndNonArchived' => $unionTable, 'filterOption' => $filterOption]);
+    }
+
+    public function showFaultsReport(){
+        return View('report.showFaultsReport');
+    }
+
+    public function getFixedAndUnFixedDamages() {
         // get all damages belonging to NON-archived bookings, and car details
         $joinTableNonArchivedBookingVehicleDamage = DB::table('bookings')
             ->join('vehicles', 'vehicles.id', '=', 'bookings.fldCarId')
@@ -50,19 +95,8 @@ class ReportController extends Controller
             ->orderBy('damageDate', 'desc')
             ->get();
 
-        if ($request->radFilterDamages == null)
-        {
-            $filterOption = "fixedAndUnFixed";
-        }
-        else
-        {
-            $filterOption = $request->radFilterDamages;
-        }
-
-        return View('report.showDamagesReport', ['unionTableArchivedAndNonArchived' => $unionTable, 'filterOption' => $filterOption]);
+        return $unionTable;
     }
 
-    public function showFaultsReport(){
-        return View('report.showFaultsReport');
-    }
+
 }
