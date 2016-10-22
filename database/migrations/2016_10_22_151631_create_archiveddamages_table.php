4355f623d5f13a7786a33c3cc5ad02446a23be23
kirsti <kirstine78@gmail.com>
2016-10-22 15:26:46 +1100

    add migration archiveddamages table

---
+++ database/migrations/2016_10_22_151631_create_archiveddamages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArchiveddamagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('archiveddamages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->mediumInteger('fldArchiveBookingNo');
+            $table->string('fldDamageType', 50);
+            $table->string('fldDamageDescription', 200);
+            $table->date('fldDamageDate');
+            $table->boolean('fldFixed');
+
+            $table->foreign('fldArchiveBookingNo')->references('id')->on('archivedbookings')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('archiveddamages');
+    }
+}
