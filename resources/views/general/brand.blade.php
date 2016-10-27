e5d40bf77774ca6394e09bfe87d38c995a61945c
kirsti <kirstine78@gmail.com>
2016-10-27 20:03:45 +1100

    brands page not finished yes

--- resources/views/general/brand.blade.php
+++ resources/views/general/brand.blade.php
@@ -14,10 +14,39 @@
     <div class="container">
         <div class="row">
             <div class="panel panel-default">
-                <div class="panel-heading">Brands</div>
+                <div class="panel-heading"><h2>Brands</h2></div>
 
                 <div class="panel-body">
-                    Brands Page Under Construction!!!
+                    <table>
+                        <tr>
+                            <td class="col-sm-3">
+                                <img src="{{URL::asset('/img/holden.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                            </td>
+                            <td class="col-sm-3">
+                                Foo bar
+                            </td>
+                            <td class="col-sm-3">
+                                <img src="{{URL::asset('/img/nissan.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                            </td>
+                            <td class="col-sm-3">
+                                Foo bar
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="col-sm-3">
+                                <img src="{{URL::asset('/img/subaru.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                            </td>
+                            <td class="col-sm-3">
+                                Foo bar
+                            </td>
+                            <td class="col-sm-3">
+                                <img src="{{URL::asset('/img/toyota.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                            </td>
+                            <td class="col-sm-3">
+                                Foo bar
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
