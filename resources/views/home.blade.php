029302f6ce6b312c629b55395db1200285272565
kirsti <kirstine78@gmail.com>
2016-10-27 19:42:33 +1100

    home page working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,33 @@
     <div class="container">
         <div class="row">
             <div class="panel panel-default">
-                <div class="panel-heading">Home</div>
+                <div class="panel-heading"><h2>Welcome to EzyHire</h2></div>
 
                 <div class="panel-body">
-                    Home Page Under Construction!!!
+                    <div class="col-sm-6">
+                        Find cheap car hire fast and hit the Australian roads happy with EzyHire.
+                        <br/>
+                        <br/>
+                        Just download EzyHire mobile app and get started.
+                        <br/>
+                        <br/>
+                        You can also call our friendly and helpful staff to make bookings.
+                        <br/>
+                        Phone: 0112233445
+                        <br/>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <img src="{{URL::asset('/img/car3.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                    </div>
+
+                    <div class="col-sm-2">
+                        <img src="{{URL::asset('/img/car2.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                    </div>
+
+                    <div class="col-sm-2">
+                        <img src="{{URL::asset('/img/car1.jpg')}}" alt="car" class="img-responsive marginTopBottom" >
+                    </div>
                 </div>
             </div>
         </div>
