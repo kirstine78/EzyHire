0bc20b7f357458241e45f97d45edb51f5c63ebe5
kirsti <kirstine78@gmail.com>
2016-10-29 11:26:32 +1100

    cleaning code for uncommented stuff

--- resources/views/customer/displayUpdateCustomerForm.blade.php
+++ resources/views/customer/displayUpdateCustomerForm.blade.php
@@ -18,16 +18,6 @@
 
                 <div class="panel-body">
 
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
                     <!--  form -->
                     <form action="{{ url('customer/update') }}" method="POST" class="form-horizontal">
 
