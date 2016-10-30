44461cb979aed6bb560578c6be2ec16294bb06d2
kirsti <kirstine78@gmail.com>
2016-10-30 16:01:01 +1100

    clean code

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -78,42 +78,6 @@ class ListController extends Controller
     }
 
 
-//    public function listDamagesByCustomerForm(){
-//
-//        // the form will need a Customer DROP DOWN list
-//        // fetch all customers that are not flagged deleted from db
-//        $cust = app('App\Http\Controllers\CustomerController')->getAllNotDeletedCustomers();
-//
-//        // make sure to pass empty $joinTable
-//        $joinTable = [];
-//
-//        // make sure to pass in null for $customerIdDropDownSelected
-//        $customerIdDropDownSelected = null;
-//
-//        return View('list.listDamagesByCustomer', ['customers' => $cust, 'joinTable' => $joinTable, 'customerIdDropDown' => $customerIdDropDownSelected]);
-//    }
-
-
-//    public function listDamagesByCustomer(Request $request){
-//
-//        // the form will need a Customer DROP DOWN list
-//        // fetch all customers that are not flagged deleted from db
-//        $cust = app('App\Http\Controllers\CustomerController')->getAllNotDeletedCustomers();
-//
-//        $customerIdDropDownSelected = $request->customer_id;
-//
-//        // create a Collection based on a join of 4 tables
-//        $joinTable = DB::table('bookings')
-//            ->join('customers', 'bookings.fldCustomerId', '=', 'customers.id')
-//            ->join('vehicles', 'vehicles.id', '=', 'bookings.fldCarId')
-//            ->join('damages', 'bookings.id', '=', 'damages.fldBookingNo')
-//            ->where('customers.id', '=', $customerIdDropDownSelected)
-//            ->orderBy('damages.fldDamageDate', 'desc')->get();
-//
-//        return View('list.listBookingsByCustomer', ['customers' => $cust, 'joinTable' => $joinTable, 'customerIdDropDown' => $customerIdDropDownSelected]);
-//    }
-
-
     /**
      * show list of damages for specific customer, excluding damages belonging to archived bookings
      * @param Request $request
