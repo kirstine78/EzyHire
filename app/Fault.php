698f45f7d97d26a53b3858026a9c0fc4cd00d880
kirsti <kirstine78@gmail.com>
2016-10-30 20:44:27 +1100

    add comments

--- app/Fault.php
+++ app/Fault.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fault extends Model
 {
+    /**
+     * relationship not being used.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function vehicle() {
 
         return $this->belongsTo(Vehicle::class, 'fldCarId');
-    }
-
-    public function vehicleNotRetired() {
-        return $this->belongsTo(Vehicle::class, 'fldCarId')->notRetired();
     }
 }
--- app/Vehicle.php
+++ app/Vehicle.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
+    /**
+     * relationship currently not used in system
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function bookings() {
-
         // writing: Booking::class   is equivalent to: 'App\Comment'
         return $this->hasMany(Booking::class, 'fldCarId');
     }
@@ -17,10 +20,15 @@ class Vehicle extends Model
         $this->attributes['updated_at'] = Carbon::now();
     }
 
+
+    /**
+     * scope currently not used in system
+     * @param $query
+     * @return mixed
+     */
     public function scopeNotRetired($query)
     {
         return $query->where('fldRetired', 0);
     }
-
 
 }
